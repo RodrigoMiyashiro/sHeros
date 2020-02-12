679a67b4e6f570122ec1c74888a0d6f853a2c213
Rodrigo Miyashiro <rodrigo.miyashiro@homerefill.com.br>
2020-02-11 22:18:12 -0300

    wip

---
+++ sHeros/sHeros/Feature/Character/List/ListCharactersInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  ListCharactersInteractor.swift
+//  sHeros
+//
+//  Created by Rodrigo Miyashiro on 25/01/20.
+//  Copyright © 2020 Rodrigo Miyashiro. All rights reserved.
+//
+
+import Foundation
---
+++ sHeros/sHeros/Feature/Character/List/ListCharactersRouter.swift
@@ -0,0 +1,9 @@
+//
+//  ListCharactersRouter.swift
+//  sHeros
+//
+//  Created by Rodrigo Miyashiro on 25/01/20.
+//  Copyright © 2020 Rodrigo Miyashiro. All rights reserved.
+//
+
+import Foundation
